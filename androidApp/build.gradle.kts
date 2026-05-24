dfcb4aae525c568e341e7f91ac0a68d41e17f529
Viacheslav Chugunov <viacheslav.chugunov@protonmail.com>
2026-05-24 16:07:29 +0300

    Android: Add release setup

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -60,7 +60,8 @@ android {
     }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
     compileOptions {
